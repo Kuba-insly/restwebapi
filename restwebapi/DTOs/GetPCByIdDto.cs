2e3fc0b9f74fba735b4e0bff0294cbad4506c637
Kuba Makolski <jakubm2004@vp.pl>
2026-05-18 20:15:03 +0200

    .

---
+++ restwebapi/DTOs/GetPCByIdDto.cs
@@ -0,0 +1,42 @@
+namespace restwebapi.DTOs;
+
+public class GetPCByIdDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public float Weight { get; set; }
+    public int Warranty { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public int Stock { get; set; }
+    public IEnumerable<PCComponentDto> Components { get; set; } = [];
+}
+
+public class PCComponentDto
+{
+    public int Amount { get; set; }
+    public ComponentDto Component { get; set; } = null!;
+}
+
+public class ComponentDto
+{
+    public string Code { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public ManufacturerDto Manufacturer { get; set; } = null!;
+    public ComponentTypeDto Type { get; set; } = null!;
+}
+
+public class ManufacturerDto
+{
+    public int Id { get; set; }
+    public string Abbreviation { get; set; } = string.Empty;
+    public string FullName { get; set; } = string.Empty;
+    public DateTime FoundationDate { get; set; }
+}
+
+public class ComponentTypeDto
+{
+    public int Id { get; set; }
+    public string Abbreviation { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+}
---
+++ restwebapi/Exceptions/NotFoundException.cs
@@ -0,0 +1,6 @@
+namespace restwebapi.Exceptions;
+
+public class NotFoundException : Exception
+{
+    public NotFoundException(string message = "Resource not found") : base(message) { }
+}
